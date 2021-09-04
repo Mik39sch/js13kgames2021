c8ea012c93a776e9a9b9aa282d473b3b76a6885a
Mik39sch <mlucky39sch@gmail.com>
2021-09-04 17:49:50 +0900

    Game over and change design

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { init, GameLoop, Sprite, SpriteSheet, keyPressed, initKeys, Text } from 'kontra';
 
-let { canvas } = init();
+let { canvas, context } = init();
 initKeys();
 
 // constant ---------------------------------------------------------
@@ -21,7 +21,7 @@ const MOVE_SPEED = 2;
 const startTime = new Date();
 
 let image = new Image();
-image.src = 'assets/imgs/character_walk_sheet.png';
+image.src = 'assets/images/player.png';
 
 const rand = (max, min) => {
     return Math.floor( Math.random() * (max + 1 - min) ) + min ;
@@ -31,65 +31,41 @@ const getTime = (time) =>
     ('' + ((time | 0) / 1000)).replace('.', ':');
 
 
-let creating = false;
-
 image.onload = function() {
-    let spriteSheet = SpriteSheet({
-        image: image,
-        frameWidth: PLAYER_WIDTH,
-        frameHeight: PLAYER_HEIGHT,
-
-        // this will also call createAnimations()
-        animations: {
-        // create 1 animation: idle
-        idle: {
-            // a single frame
-            frames: 1
-        },
-        jump: {
-            // sequence of frames (can be non-consecutive)
-            frames: [1, 10, 1],
-            frameRate: 10,
-            loop: false,
-        },
-        walk: {
-            // ascending consecutive frame animation (frames 2-6, inclusive)
-            frames: '2..6',
-            frameRate: 20
-        },
-        moonWalk: {
-            // descending consecutive frame animation (frames 6-2, inclusive)
-            frames: '6..2',
-            frameRate: 20
-        },
-        attack: {
-            // you can also mix and match, in this case frames [8,9,10,13,10,9,8]
-            frames: ['8..10', 13, '10..8'],
-            frameRate: 10,
-            loop: false,
-        }
-        }
-    });
-
-    let player = Sprite({
+    const player = Sprite({
         x: 100,
         y: 150,
-
         scaleX: 0.5,
         scaleY: 0.5,
-
+        image: image,
         anchor: {x: 0.5, y: 0.5},
-        animations: spriteSheet.animations
     });
 
-    let text = Text({
-        text: '0',
-        font: '24px Arial',
-        color: 'red',
-        x: 2,
-        y: 2,
-        textAlign: 'center'
-      });
+    let background = {
+        stars: [],
+        defineStars: function () {
+            for (let i = 0; i < 70; i++) {
+                this.stars.push([rand(0, STAGE_WIDTH), rand(0, STAGE_HEIGHT), rand(1, 3)]);
+            }
+        },
+        render: function () {
+            context.save()
+            context.beginPath();
+            context.rect(0, 0, STAGE_WIDTH, STAGE_HEIGHT);
+            context.fillStyle = "rgba(8, 6, 51, 1)";
+            context.fill();
+            context.closePath();
+
+            this.stars.forEach(star => {
+                context.beginPath();
+                context.fillStyle = 'rgba(255, 255, 255, 0.8)';
+                context.arc(star[0], star[1], star[2], 0, 2 * Math.PI);
+                context.fill();
+            });
+
+            context.restore();
+        }
+    }
 
     const obstacles = [];
 
@@ -99,11 +75,11 @@ image.onload = function() {
         const y = above ? 0 : rand(Y_LOAD_WIDTH, STAGE_HEIGHT/2);
         const width = 10;
         const height = above ? rand(STAGE_HEIGHT/2, STAGE_HEIGHT-Y_LOAD_WIDTH) : STAGE_HEIGHT - y;
-        const color = 'black';
+        const color = 'gray';
 
         const nextWidth = rand(X_LOAD_WIDTH, X_LOAD_WIDTH*4);
 
-        obstacles.push(Sprite({x, y, width, height, color, nextWidth}));
+        obstacles.push(Sprite({x, y, width, height, color, above, nextWidth}));
     };
 
     createObstacle();
@@ -119,18 +95,14 @@ image.onload = function() {
             }
             if (keyPressed('left')) {
                 player.dx = - MOVE_SPEED;
-                player.playAnimation("walk");
             } else if (keyPressed('right')) {
                 player.dx = MOVE_SPEED;
-                player.playAnimation("walk");
             }
 
             if (keyPressed('up')) {
                 player.dy = - MOVE_SPEED;
-                player.playAnimation("walk");
             } else if (keyPressed('down')) {
                 player.dy = MOVE_SPEED;
-                player.playAnimation("walk");
             }
 
             if (
@@ -139,7 +111,6 @@ image.onload = function() {
             ) {
                 player.dx = 0;
                 player.dy = 0;
-                player.playAnimation("idle");
             }
 
             if (player.x >= STAGE_WIDTH) {
@@ -164,27 +135,36 @@ image.onload = function() {
             const now = new Date();
             const ms = now.getTime() - startTime.getTime();
             const s = Math.floor(ms / 1000);
-            console.log(getTime(ms));
-            // const displayTime = getTime(diff);
+
+            const displayTime = getTime(ms);
 
             if (beforeTime != s) {
                 beforeTime = s;
-                // const scoreEl = document.getElementById("score");
-                // scoreEl.innerText(s);
-                // text.text = s;
                 if (s % 5 === 0) obstacleMoveSpeed += 0.2;
+            }
+
+            if (obstacles.length > 0) {
+                obstacles.forEach(obstacle => {
+                    if (
+                        player.x >= obstacle.x && player.x <= obstacle.x + obstacle.width &&
+                        player.y >= obstacle.y && player.y <= obstacle.y + obstacle.height
+                    ) {
+                        console.log("Game Over X");
+                        return;
+                    }
+                });
             }
         },
         render: function() {
+            background.render();
             player.render();
 
             if (obstacles.length > 0) {
                 obstacles.forEach(obstacle => obstacle.render());
             }
-
-            text.render();
         }
     });
 
+    background.defineStars();
     loop.start();
 };
